75d8faffb8eb80b8fdbe3b1f70cc8509e513e3d6
Justracy <justracy@Duyens-MacBook-Pro.local>
2020-01-16 17:53:14 -0500

    Updates

---
+++ precache-manifest.2e0ca12285ea9802e1358a17ba4cafb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d620ee6d18dd357bb6210a72bf661a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0283de30e221f0fd1e5c",
+    "url": "/monsters-rolodex/static/css/main.7d21fb59.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0283de30e221f0fd1e5c",
+    "url": "/monsters-rolodex/static/js/main.3289a0d3.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
